b1a6376c5ffaf8efdca99e05530ad74ba34e159c
dang0x5f <dannypgehrz@gmail.com>
2025-01-25 19:06:09 -0500

    add file read

--- deploy.lua
+++ deploy.lua
@@ -1,9 +1,17 @@
-#!/usr/local/bin/lua54
+#!/usr/local/bin/lua54 
 
 local crtsymlib = require "crtsymlib"
 
 home = os.getenv("HOME")
+pwd  = os.execute("pwd")
 
+file = io.open(arg[1],"r")
+if file then
+    for line in io.lines(arg[1]) do
+        print(line)
+    end
+end
+file:close()
 
 
 -- status, errno = crtsymlib.makelink("./crtsymlib.c","symlink")
