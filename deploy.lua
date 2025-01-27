21702df304bae937d48ec9d5041d1fad3316552a
dang0x5f <dannypgehrz@gmail.com>
2025-01-26 21:37:35 -0500

    added manifest parse structure

--- deploy.lua
+++ deploy.lua
@@ -3,24 +3,43 @@
 local crtsymlib = require "crtsymlib"
 
 home = os.getenv("HOME")
-pwd  = os.execute("pwd")
+
+-- repeat
+--     print(" * Symbolic Link -> " .. 
+--           " anoteuhsaou")
+--     io.write("Does this look correct? [y/n] ")
+--     input = io.read()
+-- until input ~= "" and 
+--       input == "y" or 
+--       input == "n"
+
+-- print(input)
 
 file = io.open(arg[1],"r")
 if file then
     for line in io.lines(arg[1]) do
-        -- print(line)
-        -- print(string.len(line))
-        src, dst, path = crtsymlib.split_on_comma(line,string.len(line));
-        print(src)
-        print(dst)
-        print(path)
-        -- print(dst)
-        -- print(path)
-        -- crtsymlib.split_on_comma(line);
-    end
-end
-file:close()
+        c = string.sub(line,1,1)
 
+        if c == "*" then
+            print(line:sub(2))
+        elseif c == "#" then
+            print("comment")
+        else
+            print("gtg")
+        end
+
+
+        -- src, dst, path = crtsymlib.split_on_comma(line,string.len(line));
+
+        -- print(src)
+        -- if path == "null" then
+        --     print(home .. dst)
+        -- else
+        --     print(home .. path .. dst)
+        -- end
+    end
+    file:close()
+end
 
 -- status, errno = crtsymlib.makelink("./crtsymlib.c","symlink")
 
